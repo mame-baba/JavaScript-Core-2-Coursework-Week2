ebe457362a0ef7fdddc2d8bb8bc9b4409dc454d1
mame-baba <mamebabambaye@gmail.com>
2021-01-22 01:44:18 +0000

    A-B-C
    Updated List using for each

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,18 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+  arrayOfPeople.forEach(element => {
+    var hOne = document.createElement("h1");
+    hOne.innerText = element["name"];
+    content.appendChild(hOne);
+    var hTwo = document.createElement("h2");
+    hTwo.innerText = element["job"];
+    content.appendChild(hTwo);
+
+
+  });
+  console.log(arrayOfPeople)
+
+
 }
 
 let people = [
--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,13 @@
 function shoppingList(arrayOfPeople) {
-  // Write your code here...
+  var myList = document.querySelector("#content");
+  var myNewList = document.createElement("ul");
+  arrayOfPeople.forEach(element => {
+var newShoppingList = document.createElement("li");
+newShoppingList.innerText = element;
+myNewList.appendChild(newShoppingList);
+
+  });
+  // console.log(myNewList);
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
