57f644ce03f2408d00eef721d915c7f1eb1e338c
mame-baba <mamebabambaye@gmail.com>
2021-01-24 23:00:52 +0000

    Part2/Bike-project/ Todo List
    Part2-Bike Project Updated,
    
    Todo-List (Project-Folder)

--- projects/1-bike-project/js/main.js
+++ projects/1-bike-project/js/main.js
@@ -1,40 +1,51 @@
-var blueBtn = document.getElementById("blueBtn");
+let blueBtn = document.getElementById("blueBtn");
 
- function clickedBtn1(){
-    var jumboBtn = document.querySelector(".jumbotron");
-    jumboBtn.style.backgroundColor = "#588fbd";
-    var donateBikeBtn = document.getElementById("donate-button");
-    donateBikeBtn.style.backgroundColor = "#ffa500";
-    var volunteerBtn = document.getElementById("volunteer-button");
-    volunteerBtn.style.backgroundColor = "black";
-    volunteerBtn.style.color = "white";
-
+function clickedBtn1() {
+  let jumboBtn = document.querySelector(".jumbotron");
+  jumboBtn.style.backgroundColor = "#588fbd";
+  let donateBikeBtn = document.getElementById("donate-button");
+  donateBikeBtn.style.backgroundColor = "#ffa500";
+  let volunteerBtn = document.getElementById("volunteer-button");
+  volunteerBtn.style.backgroundColor = "black";
+  volunteerBtn.style.color = "white";
 }
 // Adding EventListener
 blueBtn.addEventListener("click", clickedBtn1);
 
-var orangeButton = document.getElementById("orangeBtn");
+let orangeButton = document.getElementById("orangeBtn");
 
-function clickedBtn2(){
-    var jumboBtn = document.querySelector(".jumbotron");
-    jumboBtn.style.backgroundColor = "#f0ad4e";
-    var donateBikeBtn = document.getElementById("donate-button");
-    donateBikeBtn.style.backgroundColor = "#5751fd";
-    var volunteerBtn = document.getElementById("volunteer-button");
-    volunteerBtn.style.backgroundColor ="#8c9c08";
+function clickedBtn2() {
+  let jumboBtn = document.querySelector(".jumbotron");
+  jumboBtn.style.backgroundColor = "#f0ad4e";
+  let donateBikeBtn = document.getElementById("donate-button");
+  donateBikeBtn.style.backgroundColor = "#5751fd";
+  let volunteerBtn = document.getElementById("volunteer-button");
+  volunteerBtn.style.backgroundColor = "#8c9c08";
 }
 // adding EventListener
 orangeButton.addEventListener("click", clickedBtn2);
 
-var greenButton = document.getElementById("greenBtn");
+let greenButton = document.getElementById("greenBtn");
 
-function clickedBtn3(){
-    var jumboBtn = document.querySelector(".jumbotron");
-    jumboBtn.style.backgroundColor = "#87ca8a";
-    var donateBikeBtn = document.getElementById("donate-button");
-    donateBikeBtn.style.backgroundColor = "black";
-    var volunteerBtn = document.getElementById("volunteer-button");
-    volunteerBtn.style.backgroundColor ="#8c9c08";
+function clickedBtn3() {
+  let jumboBtn = document.querySelector(".jumbotron");
+  jumboBtn.style.backgroundColor = "#87ca8a";
+  let donateBikeBtn = document.getElementById("donate-button");
+  donateBikeBtn.style.backgroundColor = "black";
+  let volunteerBtn = document.getElementById("volunteer-button");
+  volunteerBtn.style.backgroundColor = "#8c9c08";
 }
 // adding EventListener
 greenButton.addEventListener("click", clickedBtn3);
+
+//Part 2
+let subButton = document.getElementById(submit-button);
+
+function formValidation(){
+    let formInput = document.getElementsByClassName("form-control");
+    for( var i = 0; formValidation.length > 0; i++){
+        if(!(formInput[i].value).length > 0){
+            formInput[i].style.backgroundColor = "ffb1ad";
+        }
+    }
+}
--- projects/2-todo-list/script.js
+++ projects/2-todo-list/script.js
@@ -1,7 +1,54 @@
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
-  // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+  todos.forEach((element) => {
+    let li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.classList.add("d-flex");
+    li.classList.add("justify-content-between");
+    li.classList.add("align-items-center");
+    li.innerText = element.task;
+
+    let span = document.createElement("span");
+
+    span.classList.add("badge");
+    span.classList.add("bg-primary");
+    span.classList.add("rounded-pill");
+
+    let iComplete = document.createElement("i");
+    
+    iComplete.classList.add("fa");
+    iComplete.classList.add("fa-check");
+    iComplete.setAttribute("aria-hidden", "true");
+    iComplete.innerHTML = "&nbsp";
+    iComplete.addEventListener(
+      "click",
+      (e) => {
+        e.target.parentNode.parentNode.style.textDecoration = "line-through";
+      },
+      false
+    );
+    span.appendChild(iComplete);
+    let iDelete = document.createElement("i");
+    iDelete.classList.add("fa");
+    iDelete.classList.add("fa-trash");
+    iDelete.setAttribute("aria-hidden", "true");
+    iDelete.addEventListener(
+      "click",
+      (e) => {
+        e.target.parentNode.parentNode.parentNode.removeChild(
+          e.target.parentNode.parentNode
+        );
+        // i> span> li >ul.remove child(i span li)
+      },
+      true
+    );
+    span.appendChild(iDelete);
+    li.appendChild(span);
+    list.appendChild(li);
+  });
 }
+  // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+
 
 // These are the same todos that currently display in the HTML
 // You will want to remove the ones in the current HTML after you have created them using JavaScript
